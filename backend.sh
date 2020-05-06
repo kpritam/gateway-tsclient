267dd76332cb58be07695a11691273f4a9a9ce1b
Pritam Kadam <phkadam2008@gmail.com>
2020-05-06 19:02:39 +0530

    Update script

--- backend.sh
+++ backend.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-cd ../tmt-backend && sbt tmt-backend/run
+# Starts Location, Auth and Gateway
+cd ../tmt-backend && sbt ";tmt-backend/run start -c --auth"
+
+# Starts Location, Config, Auth and Gateway with metrics enabled
+# cd ../tmt-backend && sbt ";tmt-backend/run start -c --config --auth -g --metrics"
