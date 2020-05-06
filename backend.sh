d79e8c4aae341fe6990e785e09a96f92b9694113
Pritam Kadam <phkadam2008@gmail.com>
2020-05-06 19:15:08 +0530

    Update script

--- backend.sh
+++ backend.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Starts Location, Auth and Gateway
-cd ../tmt-backend && sbt ";tmt-backend/run start -c --auth"
+cd ../tmt-backend && sbt ";tmt-backend/run start --csw-args --auth"
 
 # Starts Location, Config, Auth and Gateway with metrics enabled
-# cd ../tmt-backend && sbt ";tmt-backend/run start -c --config --auth -g --metrics"
+# cd ../tmt-backend && sbt ";tmt-backend/run start --csw-args --config --auth --gateway-args --metrics"
